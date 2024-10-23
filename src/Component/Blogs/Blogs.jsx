87398c28b7569374345799e3cc8461467485ade0
myFirstgitrepo <tanvirmahin607@gmail.com>
2024-10-23 17:01:47 +0600

    first commit

---
+++ src/Component/Blogs/Blogs.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const Blogs = () => {
+
+    const[blogs, setBlogs] = useState([]);
+
+    useEffect(()=>{
+
+        fetch('blog.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data));
+
+    }, [])
+    console.log(blogs);
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
